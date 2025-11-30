d8ff848aaa8fc0dd645d9b584a1aeef27c618368
Tripathi, Madhav <Madhav.Tripathi@Honeywell.com>
2025-11-30 22:32:36 +0530

    LOMBOK plugin

--- sb-ecom/src/main/java/com/ecommerce/project/model/Category.java
+++ sb-ecom/src/main/java/com/ecommerce/project/model/Category.java
@@ -2,34 +2,17 @@ package com.ecommerce.project.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Entity(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
     @Id
     private Long categoryId;
     private String categoryName;
 
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
-
-    public Category() {
-    }
-
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
 }
